1a60ee274a54c357014519053f36853524fdc357
TonyVu258 <thienvu258.it@gmail.com>
2023-03-02 11:40:34 +0700

    Popper and Tippy

---
+++ src/components/Layout/AccountItem/index.js
@@ -0,0 +1,22 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+    return ( 
+        <div className={cx('wrapper')}>
+            <img className={cx('avata')} src='https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/42e5b6714cea31c626b9aeb10acb6e36.jpeg?x-expires=1677902400&x-signature=Xw8xpHguR6hokfNEZqyX2pVqeCw%3D' alt="Hoaaa"/>
+            <div className={cx('info')}>
+                <h4 className={cx('name')}><span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+     );
+}
+
+export default AccountItem;
